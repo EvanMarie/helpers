7e41b654c498f945be32897245497f55dd847859
Evan Marie Carr <evancarr53@gmail.com>
2023-01-11 20:46:32 -0500

    changed center to see

---
+++ flipper.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# What does pd.pipe do?
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.pipe.html
+# https://tomaugspurger.github.io/method-chaining
+
+# divmod() takes two numbers and returns a pair of numbers (a tuple) consisting of their
+# quotient and remainder.
