e95eee9fb980952fc5123198cb317292529cc9ac
Evan Marie Carr <evancarr53@gmail.com>
2023-01-15 15:01:35 -0500

    Lots of changes to make things oh so much better

--- flipper.py
+++ flipper.py
@@ -4,5 +4,4 @@ import pandas as pd
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.pipe.html
 # https://tomaugspurger.github.io/method-chaining
 
-# divmod() takes two numbers and returns a pair of numbers (a tuple) consisting of their
-# quotient and remainder.
+
